a61a7f7795c54b761db899f79692b71fe2fe1f2d
Pamella Cathryn <pamellaexo@gmail.com>
2021-11-19 17:47:56 +0700

    Tugas Frontend 2

---
+++ src/components/AddFavorites.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AddFavorites = () => {
+    return (
+        <>
+            <span className='mr-2'>Add to Wishlist</span>
+        </>
+    );
+};
+
+export default AddFavorites;
---
+++ src/components/RemoveFavorites.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const RemoveFavorites = () => {
+    return (
+        <span className="mr-2">Remove from Wishlist</span>
+    )
+}
+
+export default RemoveFavorites;
